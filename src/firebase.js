92c047573daf833ec207c25e69874cc8c1551cfd
Shivam Shah <shiva.sean27@gmail.com>
2020-10-26 23:16:38 +0530

    After initial setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyAGIdNmGVgUQ1Q9hbBfqABxS2MM-cEcTI0',
+	authDomain: 'shivam-whatsapp.firebaseapp.com',
+	databaseURL: 'https://shivam-whatsapp.firebaseio.com',
+	projectId: 'shivam-whatsapp',
+	storageBucket: 'shivam-whatsapp.appspot.com',
+	messagingSenderId: '145061318834',
+	appId: '1:145061318834:web:40b7a1d1bc4664f65d39ab',
+};
