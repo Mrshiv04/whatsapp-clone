ec4aa5b326bbd76b40e10e0cd3f214546a736ac9
Shivam Shah <shiva.sean27@gmail.com>
2020-10-28 17:28:56 +0530

    Before reverting back to previous

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		axios.get('/messages/sync').then((response) => {
+			setMessages(response.data);
+		});
+	});
+
+	useEffect(() => {
+		const pusher = new Pusher('c72a197dd424cf0f8be3', {
+			cluster: 'ap2',
+		});
+
+		const channel = pusher.subscribe('messages');
+		channel.bind('inserted', (newMessage) => {
+			alert(JSON.stringify(newMessage));
+			setMessages([...messages, newMessage]);
+		});
+
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]);
+
+	console.log(messages);
+
 	return (
 		<div className='app'>
 			<div className='app_body'>
